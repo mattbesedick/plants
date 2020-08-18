4db888da40f78b2e972f5cace2346e3497b9db7e
Matthew Besedick <mtbesedick@gmail.com>
2020-08-18 14:12:45 -0500

    started to create firebase functions to add plants/get current plants

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,49 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin')
+const admin = require('firebase-admin');
+const account = require('./serviceAccount.json');
+const firebase = require('firebase');
 
-admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
+admin.initializeApp({
+	credential: admin.credential.cert(account),
+	databaseURL: 'https://plantsapp-4fb01.firebaseio.com'
 });
 
+firebase.initializeApp(account);
 
 exports.getPlants = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('plants').get().then(data => {
-        let plants = []
-        data.forEach(doc => {
-            plants.push(doc.data())
-        })
-        return res.json(plants)
-    })
-        .catch(err => {
-            console.error(err)
-        })
-})
+	admin
+		.firestore()
+		.collection('plants')
+		.get()
+		.then((data) => {
+			let plants = [];
+			data.forEach((doc) => {
+				plants.push(doc.data());
+			});
+			return res.json(plants);
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+});
+
+exports.createPlant = functions.https.onRequest((req, res) => {
+	if (req.method !== 'POST') {
+		return res.json({ message: `method not allowed` });
+	}
+	const newPlant = {
+		name: req.body.name
+	};
+
+	admin
+		.firestore()
+		.collection('Plants')
+		.add(newPlant)
+		.then((doc) => {
+			return res.json({ message: `document ${doc.id} created successfully` });
+		})
+		.catch((err) => {
+			res.status(500).json({ message: `something went wrong` });
+			console.error(err);
+		});
+});
